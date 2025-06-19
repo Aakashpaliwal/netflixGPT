5d1a1e13e9fee0b301432abae25d8fb6f5d1b08f
neo404 <aakashpaliwal95@gmail.com>
2025-06-19 13:30:14 +0530

    logout add

--- src/Screens/Header/Header.jsx
+++ src/Screens/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CircleCheckIcon, CircleHelpIcon, CircleIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import {
   NavigationMenu,
@@ -10,11 +11,39 @@ import {
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Link } from "react-router-dom";
 import useUserStore from "@/store/userStore";
+import { signOut } from "firebase/auth";
+import { auth } from "@/firebase";
 
 const Header = () => {
   const userName = useUserStore((state) => state.userName);
+  const setUserName = useUserStore((state) => state.setUserName)
+
+  console.log(setUserName)
+
+  const logoutHandler = async () => {
+    try {
+      const result = await signOut(auth)
+      console.log(result)
+       if(result) {
+      setUserName(null)
+    }
+    } catch (error) {
+      console.log(error.code)
+    }
+  }
+
   return (
     <header className="sticky top-0 z-50 bg-black text-white shadow-md">
       <div className="mx-auto px-4 py-3 flex justify-between items-center">
@@ -36,13 +65,36 @@ const Header = () => {
           </NavigationMenu>
         </div>
         <div className="flex">
-          <h1 className="text-xl font-bold mr-3 align-baseline">Netflix GPT</h1>
           <NavigationMenu viewport={false}>
             <NavigationMenuList>
               <NavigationMenuItem>
-                <NavigationMenuLink asChild>
-                  <Link to="/movies">{userName || "User"}</Link>
-                </NavigationMenuLink>
+                {userName && (
+                  <>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <Avatar>
+                          <AvatarImage src="https://github.com/shadcn.png" />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuPortal>
+                        <DropdownMenuContent className={"w-56 border-0"}>
+                          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                          <DropdownMenuSeparator className={'bg-gray-300'} />
+                          <DropdownMenuItem>Profile</DropdownMenuItem>
+                          <DropdownMenuItem>Billing</DropdownMenuItem>
+                          <DropdownMenuItem>Team</DropdownMenuItem>
+                          <DropdownMenuItem>Subscription</DropdownMenuItem>
+                          <DropdownMenuSeparator className={'bg-gray-300'}/>
+                          <DropdownMenuItem onClick={logoutHandler}>
+                            Log out
+                            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenuPortal>
+                    </DropdownMenu>
+                  </>
+                )}
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
