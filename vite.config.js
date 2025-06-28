050422e1a07be3556fec0774e7fe588624ad02cb
neo404 <aakashpaliwal95@gmail.com>
2025-06-28 12:48:47 +0530

    convert it into PWA

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,66 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
 import path from 'path'
+
+const manifestForPlugIn = {
+  registerType: 'autoUpdate',
+  includeAssets: [
+    'favicon.ico',
+    'apple-touch-icon.png',
+    'android-chrome-192x192.png',
+    'android-chrome-512x512.png',
+    'maskable_icon.png'
+  ],
+  manifest: {
+    name: 'React Vite App',
+    short_name: 'ReactVite',
+    description: 'A simple Vite + React PWA',
+    icons: [
+      {
+        src: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any maskable'
+      },
+      {
+        src: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon'
+      },
+      {
+        src: '/maskable_icon.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }
+    ],
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+    lang: 'en'
+  },
+  workbox: {
+    cleanupOutdatedCaches: true,
+    navigateFallback: '/index.html',
+  }
+};
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+  plugins: [react(),  tailwindcss(), VitePWA(manifestForPlugIn)],
    resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
