e206b7a629ec71fbe82072c47d0ccf19a0486f68
neo404 <aakashpaliwal95@gmail.com>
2025-06-30 11:13:06 +0530

    demo creds add

--- src/Screens/Authentication/Authentication.jsx
+++ src/Screens/Authentication/Authentication.jsx
@@ -30,6 +30,8 @@ import { useMutation } from "@tanstack/react-query";
 import { loginUser, registerUser } from "@/API/AuthApi";
 import useUserStore from "@/store/userStore";
 import axiosInstance from "@/API/interceptor";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Info } from "lucide-react";
 
 const loginSchema = z.object({
   email: z.string().email("Invalid email"),
@@ -72,7 +74,7 @@ const Authentication = () => {
   const regsiterMutation = useMutation({
     mutationFn: registerUser,
     onSuccess: (res) => {
-      setIsUserLoggedIn(1)
+      setIsUserLoggedIn(1);
       toast.success("User Registered Successfully.");
       setIsSignup(false);
     },
@@ -84,10 +86,10 @@ const Authentication = () => {
   const loginMutation = useMutation({
     mutationFn: loginUser,
     onSuccess: async (res) => {
-      setIsUserLoggedIn(1)
+      setIsUserLoggedIn(1);
       toast.success("User LoggedIn Successfully.");
       setUserName("Akash");
-       setUserImageUrl(null);
+      setUserImageUrl(null);
       navigate("/home");
       const result = await axiosInstance.get(
         "https://api.themoviedb.org/3/authentication"
@@ -107,11 +109,11 @@ const Authentication = () => {
   };
 
   const loginWithGoogleHandler = async () => {
-     const provider = new GoogleAuthProvider();
+    const provider = new GoogleAuthProvider();
     try {
       const userGoogleCred = await signInWithPopup(auth, provider);
       if (userGoogleCred) {
-        setIsUserLoggedIn(1)
+        setIsUserLoggedIn(1);
         setUserImageUrl(userGoogleCred?.user?.photoURL);
         toast.success("User LoggedIn Successfully.");
       }
@@ -121,113 +123,130 @@ const Authentication = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen m-auto w-full">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-          <CardAction>
-            <Button
-              variant="link"
-              onClick={() => {
-                setIsSignup(!isSignUp);
-              }}
-            >
-              {isSignUp ? "Log In" : "Sign Up"}
-            </Button>
-          </CardAction>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="flex flex-col gap-6">
-              {isSignUp && (
+    <>
+      <div className="w-full mt-6 p-5">
+        <Alert className="bg-blue-50 border-blue-400 text-blue-800">
+          <Info className="h-4 w-4" />
+          <AlertTitle>Demo Credentials</AlertTitle>
+          <AlertDescription className={"flex"}>
+            <span className="font-semibold">Email:</span> johndoe@yopmail.com
+            <span className="font-semibold">Password:</span> Test@123
+          </AlertDescription>
+        </Alert>
+      </div>
+
+      <div className="flex justify-center items-center h-screen m-auto w-full">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+            <CardAction>
+              <Button
+                variant="link"
+                onClick={() => {
+                  setIsSignup(!isSignUp);
+                }}
+              >
+                {isSignUp ? "Log In" : "Sign Up"}
+              </Button>
+            </CardAction>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex flex-col gap-6">
+                {isSignUp && (
+                  <div className="grid gap-2">
+                    <Label htmlFor="username">UserName</Label>
+                    <Input
+                      id="username"
+                      type="text"
+                      placeholder="Akash404"
+                      {...register("username")}
+                    />
+                    {errors.username && (
+                      <p className="text-sm text-red-500">
+                        {errors.username.message}
+                      </p>
+                    )}
+                  </div>
+                )}
                 <div className="grid gap-2">
-                  <Label htmlFor="username">UserName</Label>
+                  <Label htmlFor="email">Email</Label>
                   <Input
-                    id="username"
-                    type="text"
-                    placeholder="Akash404"
-                    {...register("username")}
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    {...register("email")}
                   />
-                  {errors.username && (
+                  {errors.email && (
                     <p className="text-sm text-red-500">
-                      {errors.username.message}
+                      {errors.email.message}
                     </p>
                   )}
                 </div>
-              )}
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  {...register("email")}
-                />
-                {errors.email && (
-                  <p className="text-sm text-red-500">{errors.email.message}</p>
-                )}
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  {!isSignUp && (
-                    <a
-                      href="#"
-                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    {!isSignUp && (
+                      <a
+                        href="#"
+                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                      >
+                        Forgot your password?
+                      </a>
+                    )}
+                  </div>
+                  <Input
+                    id="password"
+                    type="password"
+                    required
+                    {...register("password")}
+                  />
+                  {errors.password && (
+                    <p className="text-sm text-red-500">
+                      {errors.password.message}
+                    </p>
                   )}
                 </div>
-                <Input
-                  id="password"
-                  type="password"
-                  required
-                  {...register("password")}
-                />
-                {errors.password && (
-                  <p className="text-sm text-red-500">
-                    {errors.password.message}
-                  </p>
-                )}
               </div>
-            </div>
-            <Button
-              type="submit"
-              disabled={regsiterMutation?.isPending || loginMutation?.isPending}
-              className="w-full mt-7"
-            >
-              {(regsiterMutation?.isPending || loginMutation?.isPending) && (
-                <Loader2Icon className="animate-spin" />
-              )}
-              <span
-                className={`${
+              <Button
+                type="submit"
+                disabled={
                   regsiterMutation?.isPending || loginMutation?.isPending
-                    ? "hidden"
-                    : "block"
-                }`}
+                }
+                className="w-full mt-7"
               >
-                {isSignUp ? "Sign Up" : "Login"}
-              </span>
-            </Button>
-          </form>
-        </CardContent>
-        {!isSignUp && (
-          <CardFooter className="flex-col gap-2">
-            <Button
-              variant="outline"
-              className="w-full"
-              onClick={() => loginWithGoogleHandler()}
-            >
-              Login with Google
-            </Button>
-          </CardFooter>
-        )}
-      </Card>
-    </div>
+                {(regsiterMutation?.isPending || loginMutation?.isPending) && (
+                  <Loader2Icon className="animate-spin" />
+                )}
+                <span
+                  className={`${
+                    regsiterMutation?.isPending || loginMutation?.isPending
+                      ? "hidden"
+                      : "block"
+                  }`}
+                >
+                  {isSignUp ? "Sign Up" : "Login"}
+                </span>
+              </Button>
+            </form>
+          </CardContent>
+          {!isSignUp && (
+            <CardFooter className="flex-col gap-2">
+              <Button
+                variant="outline"
+                className="w-full"
+                onClick={() => loginWithGoogleHandler()}
+              >
+                Login with Google
+              </Button>
+            </CardFooter>
+          )}
+        </Card>
+      </div>
+    </>
   );
 };
 
